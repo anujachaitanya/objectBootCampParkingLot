38cf3157988f6b524ebbd9110443fe8a9e153f69
anujachaitanya <anujachaitanya09@gmail.com>
2020-12-23 09:53:54 +0530

    changed hashcode to id

--- src/parkingLot/lot/ParkingLot.java
+++ src/parkingLot/lot/ParkingLot.java
@@ -5,11 +5,13 @@ import parkingLot.ParkingLotListener;
 import java.util.HashMap;
 
 public class ParkingLot {
+    private final int id;
     private final int capacity;
     private int occupiedSpaces;
     private final HashMap<ParkingLotListener, Integer> listener;
 
-    public ParkingLot(int capacity) {
+    public ParkingLot(int id, int capacity) {
+        this.id = id;
         this.capacity = capacity;
         this.occupiedSpaces = 0;
         this.listener = new HashMap<>();
@@ -31,13 +33,13 @@ public class ParkingLot {
     private void informListeners() {
         this.listener.forEach((parkingLotListener, threshold) -> {
             if(hasPassesThreshold(threshold)){
-                parkingLotListener.listen(this.hashCode(), threshold);
+                parkingLotListener.listen(this.id, threshold);
             }
         });
     }
 
     private boolean hasPassesThreshold(Integer threshold) {
-        return ((threshold * this.capacity)  / 100) <= occupiedSpaces;
+        return ((threshold * this.capacity)  / 100) == occupiedSpaces;
     }
 
     public boolean isFull() {
--- test/parkingLot/ParkingLotTest.java
+++ test/parkingLot/ParkingLotTest.java
@@ -10,46 +10,46 @@ import static org.mockito.Mockito.verify;
 public class ParkingLotTest {
     @Test
     public void shouldParkCarInEmptyParkingSpace()  {
-        ParkingLot parkingLot = new ParkingLot(20);
+        ParkingLot parkingLot = new ParkingLot(1, 20);
         assertTrue(parkingLot.park());
     }
 
     @Test
     public void shouldNotParkCarWhenParkingLotIsFull() {
-        ParkingLot parkingLot = new ParkingLot(0);
+        ParkingLot parkingLot = new ParkingLot(1, 0);
         assertFalse(parkingLot.park());
     }
 
     @Test
     public void shouldValidateIfParkingLotIsFull() {
-        ParkingLot parkingLot = new ParkingLot(0);
+        ParkingLot parkingLot = new ParkingLot(0, 0);
         assertTrue(parkingLot.isFull());
     }
 
     @Test
     public void shouldValidateIfParkingLotIsNotFull() {
-        ParkingLot parkingLot = new ParkingLot(1);
+        ParkingLot parkingLot = new ParkingLot(1, 1);
         assertFalse(parkingLot.isFull());
     }
 
     @Test
     public void shouldUpdateTheAssistantWhenParked() {
         Assistant assistant = mock(Assistant.class);
-        ParkingLot parkingLot = new ParkingLot(1);
+        ParkingLot parkingLot = new ParkingLot(1, 1);
         parkingLot.addListener(assistant,100);
         parkingLot.park();
-        verify(assistant).listen(parkingLot.hashCode(),100);
+        verify(assistant).listen(1,100);
     }
 
     @Test
     public void shouldUpdateManagerWhenLotIs80PercentFull() {
         ParkingLotListener manager = mock(ParkingLotListener.class);
-        ParkingLot parkingLot = new ParkingLot(5);
+        ParkingLot parkingLot = new ParkingLot(1, 5);
         parkingLot.addListener(manager, 80);
         parkingLot.park();
         parkingLot.park();
         parkingLot.park();
         parkingLot.park();
-        verify(manager).listen(parkingLot.hashCode(), 80);
+        verify(manager).listen(1, 80);
     }
 }
